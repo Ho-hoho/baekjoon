bad0f5bf59d02af891b76ab38cf269417bf9fb01
Ho-hoho <80965670+Ho-hoho@users.noreply.github.com>
2021-03-23 23:33:32 +0900

    Update 11084.c

--- 11084.c
+++ 11084.c
@@ -1,26 +1,46 @@
 #include <stdio.h>
-long long dp[101] = {0};
 
-int stair(int n, int k){
-	if(n==0) return 0;
-	if(n==1) return 1;
+long long dp [101][10]={0};
+
+long long stair(int n, int k){
+	if(n==1){
+			dp[n][k] =1;
+			return 1;	
+	}
+	if(dp[n][k] != 0) return dp[n][k]%1000000000;
+	int result;
 	
-	if(k == 0 )
-		return stair(n-1,k+1);
-	if(k == 9)
-		return stair(n-1,k-1);
-	else	
-		return stair(n-1,k-1)+stair(n-1,k+1);
-	 
+	if(k==0) 
+		dp[n][k] = stair(n-1,k+1)%1000000000;
+	else if(k==9)
+		dp[n][k] = stair(n-1,k-1)%1000000000;
+	else
+		dp[n][k] = (stair(n-1, k+1)+stair(n-1,k-1))%1000000000;
+	
+	return dp[n][k];
 }
 
 int main(void) {
-	int n, result;
+	int n;
+	long long sum=0;
 	scanf("%d",&n);
 	
-	int result;
-	for(int i = 1; i<9;i++)
-		result += stair(n,9);
+	for(int i=1;i<10;i++)
+		dp[1][i] = 1;
+	stair(n,0);	
 	
+	for(int i=1; i<10 ;i++){
+		//sum = (sum+stair(n,i))%1000000000;
+		sum += stair(n,i)%1000000000;
+	}
+	
+/*
+	for(int i=1 ; i <= n; i++){
+		for(int j=0; j < 10; j++)
+			printf("%d ",dp[i][j]);
+		printf("\n");
+	}
+*/
+	printf("%lld",sum%1000000000);
 	return 0;
 }
