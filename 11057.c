6d0981df746ec0cf0d2be70400f2ab9d45462732
Ho-hoho <80965670+Ho-hoho@users.noreply.github.com>
2021-03-24 22:39:10 +0900

    Update 11057.c
    컴파일러에서는 어떻게 dp[-1][k] 가 0이였지만 백준은 아니였던듯
    답은 다 똑같은데 안됐음

--- 11057.c
+++ 11057.c
@@ -8,19 +8,22 @@ int main (){
 	for(int i=0;i<10;i++)
 		dp[1][i] = 1;
 	
-	for(int i = 0; i<10;i++)
-		for(int j = i ; j<10 ; j++)
-			dp[i][j] += dp[i-1][j]; 
-  //fix here
-		
+	for(int i = 1; i<=n;i++)
+		for(int j = 0 ; j<10 ; j++)
+			for(int k=j ; k<10 ;k++)
+				dp[i][j] = (dp[i][j] + dp[i-1][k])%10007;
+/*		
 	for(int i=1 ; i <= n; i++){
 		for(int j=0; j < 10; j++)
 			printf("%d ",dp[i][j]);
 		printf("\n");
-	}	
-	
+	}
+	for(int j=0; j < 10; j++)
+		printf("%d ",dp[n][j]);	
+*/	
+
 	for(int i = 0 ; i<10;i++)
-		sum += dp[n][i];
+		sum = (sum + dp[n][i]);
 	
 	printf("%d", sum%10007);
 	
