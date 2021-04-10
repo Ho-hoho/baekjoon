aeb1f75c52b02a4d0da082e776c59a34e132eda5
Ho-hoho <80965670+Ho-hoho@users.noreply.github.com>
2021-04-10 22:29:21 +0900

    Update 13904.c

--- 13904.c
+++ 13904.c
@@ -3,6 +3,9 @@
 int arr[1001] ={0};
 int d[1001] ={0};
 int w[1001] ={0};
+int max (int a, int b){
+	return a > b ? a : b ;
+}
 
 int main (){
 	int n;
@@ -15,22 +18,36 @@ int main (){
 	int maxidx =0;
 	for(int times =0; times <n; times++){
 		int i;
+		int maxw =0;
 		for (i=0; i<n ;i++){
-			if(maxidx < w[i])
+			if(maxw < w[i]){
+				maxw = w[i];
 				maxidx = i;
+			}
 		}
-		for(int k= maxidx ;k > 0; k--)
-			if (w[i] > arr[k] )
-				arr[k] = w[i];
-		d[i]= 0;
-		w[i]=0;
-		maxidx =0;
+		//printf("%d %d %d\n",maxidx, maxw,w[maxidx]);
 		
+		for(int k= d[maxidx]-1 ;k >= 0; k--){
+			//printf("here\n");
+			if (w[maxidx] > arr[k] ){
+				arr[k] = w[maxidx];
+				//printf("%d ",arr[k]);
+				break;
+			}
+		}
+		d[maxidx]= 0;
+		w[maxidx]=0;
+		//printf("%d clear\n",maxidx);
+		maxidx =0;
 	}
 	int sum =0;
-	for(int i=0;i<n;i++)
+	
+	//for(int i=0;i<30;i++)
+	//	printf("%d ", arr[i] );
+	
+	
+	for(int i=0;i<1001;i++)
 		sum += arr[i];
 		
 	printf("%d",sum);
 }
-
