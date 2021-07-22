b28932eded100bc9df2bd63daf61a36d9f6c8c06
Ho-hoho <80965670+Ho-hoho@users.noreply.github.com>
2021-07-22 16:17:21 +0900

    Create NM4.c

---
+++ NM4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int N, M;
+int arr[10] = { 0 };
+int visited[10] = { 0 };
+
+int promising(int num, int count) {
+	if (num <= N && count <= M)
+		return 1;
+	else return 0;
+}
+
+int getarr(int num, int count) {
+	if (promising(num, count)) {
+		if (count == M) {
+			for (int idx = 1; idx <= M; idx++) {
+				printf("%d ", arr[idx]);
+			}
+			printf("\n");
+		}
+		else {
+			for (int i = 1; i <= N; i++) {
+				if (i >= arr[count]) {
+					arr[count + 1] = i;
+					getarr(num + 1, count + 1);
+				}
+			}
+		}
+	}
+	return 0;
+}
+int main() {
+	scanf("%d %d", &N, &M);
+
+	getarr(0, 0);
+
+	return 0;
+}
