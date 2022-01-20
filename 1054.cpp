052469d48fea5ede9e1bde4a061cd41e5cffc9cf
Ho-hoho <80965670+Ho-hoho@users.noreply.github.com>
2022-01-20 18:17:25 +0900

    Create 1054.cpp

---
+++ 1054.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+int N, K;
+int countBits(int value) {
+	int count = 0;
+	while (value > 0) {
+		if ((value & 1) == 1)
+			count++;
+		value = value >> 1;
+	}
+	return count;
+}
+
+void solve() {
+	for (int i = 0;; i++) {
+		if (countBits(N) <= K) {
+			cout << i;
+			return;
+		}
+		N++;
+	}
+}
+
+int main() {
+	cin >> N >> K;
+	solve();
+}
