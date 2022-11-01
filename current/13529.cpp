b153fc791d174a83f12848a5e2cfcce09c2af643
Ho-hoho <kennyya@naver.com>
2022-11-01 16:32:49 +0900

    1101

--- current/13529.cpp
+++ current/13529.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-#define INF 987654321;
+#define MAX 100001
 typedef pair<int, int> point;
 
-int dist[400010];
+bool vis[100001];
 int N, K;
 
 void solve()
 {
-    int res = INF;
     priority_queue<point, vector<point>, greater<point> > pq;
     pq.push(make_pair(0, N));
-    dist[N] = 0;
+    vis[N] = true;
     while (!pq.empty())
     {
         point top = pq.top();
@@ -21,35 +20,34 @@ void solve()
         int cur = top.second;
         if (cur == K)
         {
-            res = cost;
+            cout << cost;
             break;
         }
         if (cur < 2 * K)
         {
-            if(dist[cur - 1] > cost + 1){
-                dist[cur - 1] = cost + 1;
-                pq.push(make_pair(cost + 1, cur - 1));
-            }
-            if(dist[cur + 1] > cost + 1){
-                dist[cur + 1] = cost + 1;
-                pq.push(make_pair(cost + 1, cur + 1));
-            }
-            if(dist[cur * 2] > cost){
-                dist[cur * 2] = cost;
+            if (cur * 2 < MAX && !vis[cur * 2])
+            {
+                vis[cur * 2] = true;
                 pq.push(make_pair(cost, cur * 2));
             }
-        
+            if (cur - 1 >= 0 && !vis[cur - 1])
+            {
+                vis[cur - 1] = true;
+                pq.push(make_pair(cost + 1, cur - 1));
+            }
+            if (cur + 1 < MAX && !vis[cur + 1])
+            {
+                vis[cur + 1] = true;
+                pq.push(make_pair(cost + 1, cur + 1));
+            }
         }
     }
-    cout << res;
 }
 int main()
 {
     cin >> N >> K;
-    for(int i = 0 ; i <= 2* K;i++)
-        dist[i] = INF;
-    if(N >= K)
-        cout << N - K;
+    if (N >= K)
+         cout << N - K;
     else
         solve();
 }
